f2eeab638b66eb887bf4cedc7330683134f697c0
Gal Rogozinski <gal.rogozinski@iota.org>
2022-06-26 11:58:08 +0300

    attempt to fix webpack issues

--- next-js/next.config.js
+++ next-js/next.config.js
@@ -1,3 +1,33 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push({
+      test: /\.node$/,
+      loader: "node-loader",
+    })
+    // config.target = "node"
+    // config.node = { __dirname: false }
+    config.resolve.alias["@sentry/node"] = "@sentry/browser"
+
+    return config
+  },
 }
+
+const nodeLoader = {
+  target: "node",
+  node: {
+    __dirname: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.node$/,
+        loader: "node-loader",
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
