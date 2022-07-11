9634d7b62131f79d9fcd7ce8e2017a6c5a69d160
Gal Rogozinski <galrogogit@gmail.com>
2022-07-11 15:52:25 +0300

    stop using webpack configs

--- next-js/next.config.js
+++ next-js/next.config.js
@@ -4,50 +4,6 @@ const nodeExternals = require("webpack-node-externals")
 
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.module.rules.push({
-      test: /\.node$/,
-      loader: "node-loader",
-    })
-    // config.plugins.push(new NodePolyfillPlugin())
-    // config.externalsPresets = { node: true }
-    // config.externals = [nodeExternals()]
-    // config.node = { __dirname: false }
-    config.resolve.alias["@sentry/node"] = "@sentry/browser"
-    config.resolve.modules.concat(["node_modules"])
-    config.resolve.fallback = {
-      fs: false,
-      stream: false,
-      crypto: false,
-      cryptojs: false,
-      os: false,
-      readline: false,
-      ejs: false,
-      assert: require.resolve("assert"),
-      path: false,
-      process: false,
-      require: false,
-      circomlib: false,
-      eddsa: false,
-    }
-
-    return config
-  },
-}
-
-const nodeLoader = {
-  target: "node",
-  node: {
-    __dirname: false,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.node$/,
-        loader: "node-loader",
-      },
-    ],
-  },
 }
 
 module.exports = nextConfig
